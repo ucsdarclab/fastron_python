07553aaa2e9703968bb32e8e290acfa5e4d206dd
mingweiXu <xumw1996@gmail.com>
2019-04-24 16:37:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ from Cython.Build import cythonize
 
 import eigency
 
-# extensions = [
-#     Extension("*", ["*.pyx"],
-#         include_dirs = eigency.get_includes()
-#     ),
-# ]
 
 extensions = [
     Extension("fastronWrapper.fastronWrapper", ["fastronWrapper/fastronWrapper.pyx"],
@@ -22,13 +17,3 @@ dist = setup(
     ext_modules = cythonize(extensions),
     #packages = ["fastronWrapper"]
 )
-
-# extensions = [
-#     Extension("fastronWrapper.fastronWrapper", ["fastronWrapper/fastronWrapper.pyx"],
-#     language="c++",
-#     include_dirs = eigency.get_includes()
-#     )]
-
-# setup(ext_modules=cythonize(extensions))
-
-# python setup.py build_ext --inplace
